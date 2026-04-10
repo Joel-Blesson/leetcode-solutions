ce1c0f431065c803461867374006c0bd95af22c3
Joel Blesson <joelblesson03@gmail.com>
2026-04-10 21:08:05 +0530

    Create 217-Contains-Duplicate.java

---
+++ java/217-Contains-Duplicate.java
@@ -0,0 +1,18 @@
+import java.util.HashMap;
+import java.util.HashSet;
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        HashSet<Integer> set = new HashSet<>();
+        for(int i=0; i < nums.length; i++){
+            int current = nums[i];
+            if(set.contains(current)){
+                return true;
+            }
+            set.add(current);
+            
+        }
+        return false;
+    }
+}
+
+        
